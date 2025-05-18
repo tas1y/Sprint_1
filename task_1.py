40164c479d5aba0b3d2d0f376fb97ea0f0602978
tas <sparke1542@yandex.ru>
2025-05-18 20:47:11 +0500

    First sprint

---
+++ task_1.py
@@ -0,0 +1,16 @@
+time_str = '1h 45m,360s,25m,30m 120s,2h 60s'
+total_minutes = 0
+
+for time_part in time_str.split(','):
+    for component in time_part.split():
+        if 'h' in component:
+            hours = int(component.replace('h', ''))
+            total_minutes += hours * 60
+        elif 'm' in component:
+            minutes = int(component.replace('m', ''))
+            total_minutes += minutes
+        elif 's' in component:
+            seconds = int(component.replace('s', ''))
+            total_minutes += seconds // 60
+
+print(f"Общее количество минут: {total_minutes}")
