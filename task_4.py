c72c6e13d71ad17fc183a6857c35d0f561b633b5
tas <sparke1542@yandex.ru>
2025-05-20 12:17:48 +0500

    first sprint

--- task_4.py
+++ task_4.py
@@ -1,5 +1,5 @@
 new_tasks = ['task_001', 'task_011', 'task_007', 'task_015', 'task_005']
-completed_tasks = ['task_002', 'task_012', 'task_006'] 
+completed_tasks = ['task_002', 'task_012', 'task_006']
 
 completed_tasks.append(new_tasks.pop())
 new_tasks.remove('task_007')
--- task_5.py
+++ task_5.py
@@ -16,7 +16,6 @@ class TestCase:
     def get_test_case(self):
         print({"Шаги": self.steps, "Ожидаемый результат": self.result})
 
-
 test_case_1 = TestCase()
 test_case_1.set_step(1, 'Перейти на сайт')
 test_case_1.set_step(3, 'Перейти в раздел Товары')
